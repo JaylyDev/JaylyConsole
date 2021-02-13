0350fe4653b3fee8e029301c9b0b68af75d59208
JaylyDev <65847850+JaylyDev@users.noreply.github.com>
2021-02-13 17:45:19 +0000

    Add files via upload

---
+++ System/Profiles/login.py
@@ -0,0 +1,8 @@
+# Name: Jayly Console
+# Credit: JaylyMC
+# Console Version: 1.4
+# Login Service Version: 4.0
+While True:
+    print("Please enter the username of this snapshot to open this document")
+    clientUsername = input()
+    
