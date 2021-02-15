0db979ef67bc0aa04808378760ea237f121082b2
JaylyDev <65847850+JaylyDev@users.noreply.github.com>
2021-02-15 16:16:41 +0000

    Update JCLauncher.py

--- JCLauncher.py
+++ JCLauncher.py
@@ -1,1 +1,12 @@
+# Developer: JaylyDev
+# Connection:
+# YouTube: https://youtube.com/jaylymc
+# Twitter: https://twitter.com/Jayly_
+# Discord: Jayly#1397
+# GitHub: https://github.com/JaylyDev/JaylyConsole
 
+# License management
+jclicense_14 = """
+
+"""
+license_file = open("LICENSE", "r")
