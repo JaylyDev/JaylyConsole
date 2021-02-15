95e34bd4c9e068de4baba1f9032c5525d75ea2d7
JaylyDev <65847850+JaylyDev@users.noreply.github.com>
2021-02-15 15:50:27 +0000

    Delete System directory

--- System/Profiles/login.py
+++
@@ -1,8 +0,0 @@
-# Name: Jayly Console
-# Credit: JaylyMC
-# Console Version: 1.4
-# Login Service Version: 4.0
-While True:
-    print("Please enter the username of this snapshot to open this document")
-    clientUsername = input()
-    
