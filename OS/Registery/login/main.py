207ac72ba11ff9d656b2132e437a52e261195bc1
Jayly <65847850+JaylyDev@users.noreply.github.com>
2021-02-22 10:34:38 +0000

    Update main.py

--- OS/Registery/login/main.py
+++ OS/Registery/login/main.py
@@ -1,4 +1,5 @@
 import json
+import os
 import os.path
 
 if os.path.isfile('profiles.json'):
@@ -9,11 +10,20 @@ if os.path.isfile('profiles.json'):
             jc_client_username = input("Enter your username here: ")
             jc_client_password = input("Enter your password here: ")
             jc_client_username = jc_client_username.lower()
-            for users in jc_profiles['profiles']:
-                if users['username'] == jc_client_username:
-                    print("Found a match of your username.")
-                    print(jc_client_username)
-                    for passwords in jc_client:
+            for client in jc_profiles['profiles']:
+                if client['username'] == jc_client_username:
+                    user_password = return client['password']
+                    if user_password == jc_client_password:
+                        print("Correct login details")
+                        print("Welcome " + jc_client_username)
+                        print("Preparing your console...")
+                        # Program redirects you to "JaylyConsole/Desktop/" folder
+                        root_path = '/JaylyConsole/OS/Console/User/'
+                        folders = ['Folder_1','Folder_x','Folder_y']
+                        for folder in folders:
+                            os.mkdir(os.path.join(root_path,folder))
+                    else:
+                        print("Incorrect username or password.")
                 else:
                     print("Incorrect username or password.")
         except:
