d8cbd425a90f9eccf6a96c7610fd66fa01700989
JaylyDev <65847850+JaylyDev@users.noreply.github.com>
2021-02-22 10:03:11 +0000

    Update main.py

--- OS/Registery/login/main.py
+++ OS/Registery/login/main.py
@@ -4,8 +4,19 @@ import os.path
 if os.path.isfile('profiles.json'):
     print ("User profiles exist")
     with open('profiles.json') as jc_profiles_file:
-    jc_profiles = json.load(jc_profiles_file)
-    for users in jc_profiles['profile']:
-        
+        try:
+            jc_profiles = json.load(jc_profiles_file)
+            jc_client_username = input("Enter your username here: ")
+            jc_client_password = input("Enter your password here: ")
+            jc_client_username = jc_client_username.lower()
+            for users in jc_profiles['profiles']:
+                if users['username'] == jc_client_username:
+                    print("Found a match of your username.")
+                    print(jc_client_username)
+                    for passwords in jc_client:
+                else:
+                    print("Incorrect username or password.")
+        except:
+            print("Unable to convert profiles data to JSON")
 else:
     print ("User profiles do not exist")
