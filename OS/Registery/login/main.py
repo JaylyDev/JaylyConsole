b41518c0469a0a048f343fa178060feeec35a078
JaylyDev <65847850+JaylyDev@users.noreply.github.com>
2021-02-22 09:29:58 +0000

    Update main.py

--- OS/Registery/login/main.py
+++ OS/Registery/login/main.py
@@ -1,1 +1,11 @@
 import json
+import os.path
+
+if os.path.isfile('profiles.json'):
+    print ("User profiles exist")
+    with open('profiles.json') as jc_profiles_file:
+    jc_profiles = json.load(jc_profiles_file)
+    for users in jc_profiles['profile']:
+        
+else:
+    print ("User profiles do not exist")
